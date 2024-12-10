26ef9e696490baf11ab092dae216be111c30845d
Erick Alvarez <erick.alvarez@live.com>
2024-12-09 16:48:41 -0800

    remove rr folder

--- rr/app/routes/_auth.dashboard._index.tsx
+++
@@ -1,3 +0,0 @@
-export default function Dashboard() {
-  return <h1>dashboard page</h1>;
-}
--- rr/app/types/actionResponse.ts
+++
@@ -1,6 +0,0 @@
-interface ActionResponse<DataType = any, ErrorType = any> {
-  data: DataType;
-  error: ErrorType;
-}
-
-export type { ActionResponse };
